c14f0c765c07f1c00fc53ec189c177e29ebeb9eb
starkchloe97 <starkchloe97@gmail.com>
2026-09-17 22:53:50 +0500

    Fix widget drag lifecycle

--- src/components/WidgetPanel.vue
+++ src/components/WidgetPanel.vue
@@ -1,10 +1,14 @@
 <script setup>
-import { WIDGETS, addWidget, state } from '../state.js'
+import { WIDGETS, addWidget, beginWidgetDrag, endDrag } from '../state.js'
 
 function onDragStart(event, type) {
+  beginWidgetDrag(type)
   event.dataTransfer.effectAllowed = 'copy'
-  event.dataTransfer.setData('application/x-vue-page-builder-widget', type)
-  event.dataTransfer.setData('text/plain', type)
+  event.dataTransfer.setData('text/plain', `widget:${type}`)
+}
+
+function onDragEnd() {
+  endDrag()
 }
 </script>
 
@@ -13,22 +17,12 @@ function onDragStart(event, type) {
     <div class="panel-head"><h2>Widgets</h2></div>
     <div class="panel-scroll">
       <div class="widget-grid">
-        <div
-          class="widget-card"
-          v-for="w in WIDGETS"
-          :key="w.type"
-          draggable="true"
-          @dragstart="onDragStart($event, w.type)"
-          @dragend="state.dragOverId = null"
-          @click="addWidget(w.type)"
-        >
+        <div v-for="w in WIDGETS" :key="w.type" class="widget-card" draggable="true" @dragstart="onDragStart($event, w.type)" @dragend="onDragEnd" @click="addWidget(w.type)">
           <div class="w-icon">{{ w.icon }}</div>
           <div class="w-label">{{ w.label }}</div>
         </div>
       </div>
-      <div class="hint-box">
-        Click a widget to add it. Or drag a widget onto the canvas or inside a section.
-      </div>
+      <div class="hint-box">Click a widget to add it. Or drag a widget onto the canvas or inside a section.</div>
     </div>
   </div>
 </template>
